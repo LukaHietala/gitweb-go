a8f4b5bcdaaac6ef042ae1cc0192435df2e3fc33
lukahietala <luka.hietala08@gmail.com>
2025-03-23 15:18:14 +0200

    add yaml, env, flag config options

--- README.md
+++ README.md
@@ -2,6 +2,40 @@
 
 Simple web interface for git server that allows inspection of repositories, branches, commits, and files. This is a personal side project to learn more about git and golang.
 
+#### Usage
+
+Run the application:
+
+```bash
+# with default settings
+./gitweb-go
+
+# with custom configuration file
+./gitweb-go -config path/to/config.yml
+
+# with environment variables
+export REPOS_PATH=/path/to/repositories
+export PORT=3000
+./gitweb-go
+```
+
+Access the web interface at `http://localhost:8080` (or your configured port).
+
+#### Configuration
+
+Configuration options:
+
+```yaml
+port: 8080 # Default: 8080
+repos_path: ./repos # Default: ./repos
+```
+
+Configuration priority:
+
+1. Environment variables (REPOS_PATH, PORT)
+2. Configuration file (specified with -config flag)
+3. Default values
+
 #### Setting up a Git Server
 
 On Debian-based systems:
@@ -26,3 +60,7 @@ For more details, refer to the [official Git documentation](https://git-scm.com/
 #### Virtualization
 
 If you don't have a separate device to host your Git server, using a VM is a good alternative. You can use a VM such as qemu/kvm with Ubuntu 24.04, as I did. Simply connect to the VM via SSH using its IP address to interact with your Git server.
+
+#### TODO
+
+-   Yaml config groups and options as the app grows
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Templates struct {
@@ -19,9 +23,51 @@ func (t *Templates) RenderTemplate(w http.ResponseWriter, name string, data inte
     t.templates.ExecuteTemplate(w, name, data)
 }
 
+type Config struct {
+    ReposPath string `yaml:"repos_path"`
+    Port      string `yaml:"port"`
+}
+
 func main() {
 	t := &Templates{}
 	t.LoadTemplates()
+
+	// default
+	config := Config{
+		Port: "8080",
+		ReposPath: "./repos",
+	}
+
+	// if config file exists, override default one
+	var configPath string
+	flag.StringVar(&configPath, "config", "config.yml", "Path to configuration file")
+	flag.Parse()
+
+	if _, err := os.Stat(configPath); err == nil {
+		data, err := os.ReadFile(configPath)
+		if err != nil {
+			log.Printf("Error reading config file: %v", err)
+		} else {
+			if err := yaml.Unmarshal(data, &config); err != nil {
+				log.Printf("Error parsing config file: %v", err)
+			} else {
+				log.Printf("Loaded configuration from %s", configPath)
+			}
+		}
+	}
+
+	// if environment variables are set, override config and default values :)
+
+	if reposPath := os.Getenv("REPOS_PATH"); reposPath != "" {
+		config.ReposPath = reposPath
+		log.Printf("Overriding repos path with env variable: %s\n", config.ReposPath)
+	}
+	if port := os.Getenv("PORT"); port != "" {
+		config.Port = port
+		log.Printf("Overriding port with env variable: %s\n", config.Port)
+	}
+
+	log.Printf("Starting server on :%s with repos path %s\n", config.Port, config.ReposPath)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t.RenderTemplate(w, "home.html", nil)
